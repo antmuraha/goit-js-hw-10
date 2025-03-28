2619ad4276f662fe8748893c057d4f87337a27a1
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-03-28 21:22:18 +0200

    task-2: fix Storage class to use private field for items and improve removeItem method

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,25 @@
 class Storage {
+  #items = [];
+
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this.items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    this.items = this.items.filter(item => item !== itemToRemove);
+    const itemExists = this.#items.includes(itemToRemove);
+    if (!itemExists) {
+      console.log(`Item "${itemToRemove}" not found.`);
+      return;
+    }
+    this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
 
